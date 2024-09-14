43c925b3d61c0f76b1cea02bf47e095bc1061719
Jobaer Islam <jobaerislam16@gmail.com>
2024-09-14 07:50:11 +0600

    Creating an Instance of the Outer Class
    When you have a non-static inner class, you need an instance of the outer class to create an instance of the inner class.

--- OOP/getterSetters.java
+++ OOP/getterSetters.java
@@ -2,13 +2,14 @@ package OOP;
 
 public class getterSetters {
     public static void main(String[] args) {
-        Pen p1 = new Pen();
+        getterSetters gs = new getterSetters();
+        Pen p1 = gs.new Pen();
         p1.setColor("Blue");
         p1.setTip(5);
         System.out.println(p1.getColor());
         System.out.println(p1.getTip());
     }
-    static class Pen{
+    class Pen{
         private String color;
         private int tip;
         String getColor() {
