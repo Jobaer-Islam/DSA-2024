38d3b62eca1b72d2e0d2b8e219fc9364fdcb9d5a
Jobaer Islam <jobaerislam16@gmail.com>
2024-09-28 16:56:36 +0600

    ..

--- Recursion/P14_BinaryStrings.java
+++ Recursion/P14_BinaryStrings.java
@@ -2,10 +2,16 @@ package Recursion;
 
 public class P14_BinaryStrings {
     public static void printBinaryString( int n, int lp, String str) {
-
-
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+        printBinaryString(n - 1, 0, str+"0");
+        if(lp == 0){
+            printBinaryString(n - 1, 1, str+"1");
+        }
     }
     public static void main(String[] args) {
-
+        printBinaryString(3, 0, " ");
     }
 }
